55c7244413c8d3569aaf3313e1864039ba017b18
lcagramos <lcagramos@carc.pshs.edu.ph>
2026-03-06 15:08:39 +0800

    updated menu choice 1 (start game)

--- Paithon's Gacha! - Abalos, Agramos, Baguis.py
+++ Paithon's Gacha! - Abalos, Agramos, Baguis.py
@@ -7,7 +7,7 @@ with open("items.json", "r") as f:
 with open("inventory.json", "r") as f:
     data = json.load(f)
 
-name = input("Welcome to Paithon's Gacha! What is your name, traveler?")
+name = input("Welcome to Paithon's Gacha! What is your name, traveler? ")
 
 print("\n\n")
 print(" .-.---------------------------------.-.")
@@ -30,12 +30,36 @@ print(" '-'----------------------------------'")
 
 
 print("\n\n")
+menuChoice = ""
+while True:
+    try:
+        menuChoice = int(input("\nEnter choice (1-4): "))
+        if 1 <= menuChoice <= 4:
+            break
+        else:
+            print("Enter a number between 1 to 4.")
+    except ValueError:
+        print("Invalid input! Try again.")
 
-menuChoice = int(input("Choice: "))
 if menuChoice == 1:
-    print("welcome")
+    print("Paithon: Hello player! In this game, you must play Python-coding related minigames and answer questions"
+        "\nto obtain a special currency called Primogems in order to wish on the current banner. You need 1,600 "
+        "\nPrimogems to wish. Good luck!")
+
+    print("1. Easy (Formative Assessment)\n2. Medium (Alternative Assessment)\n3. Hard (Summative Assessment)")
+    categoryChoice = ""
+    while True:
+        try:
+            categoryChoice = int(input("\nChoose a category (1-3)"))
+            if 1 <= menuChoice <= 3:
+                break
+            else:
+                print("Enter a number between 1 to 3.")
+        except ValueError:
+            print("Invalid input! Try again.")
 elif menuChoice == 2:
     print("")
+
 elif menuChoice == 3:
     print("╔╦╗╔═╗╦╔╗╔  ╔═╗╦═╗╔═╗╔═╗╦═╗╔═╗╔╦╗╔╦╗╔═╗╦═╗╔═╗ O")
     time.sleep(1)
