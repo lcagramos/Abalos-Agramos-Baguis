eff65eafdb4418207fc1088669eafe65ad06d856
8-Adelfa <msabalos@carc.pshs.edu.ph>
2026-03-06 14:50:27 +0800

    Scrolling ASCII credits

--- Paithon's Gacha! - Abalos, Agramos, Baguis.py
+++ Paithon's Gacha! - Abalos, Agramos, Baguis.py
@@ -1,9 +1,10 @@
 import json
+import time
 
 with open("items.json", "r") as f:
     item = json.load(f)
 
-with open("inventory.json.json", "r") as f:
+with open("inventory.json", "r") as f:
     data = json.load(f)
 
 name = input("Welcome to Paithon's Gacha! What is your name, traveler?")
@@ -11,7 +12,7 @@ name = input("Welcome to Paithon's Gacha! What is your name, traveler?")
 print("\n\n")
 print(" .-.---------------------------------.-.")
 print(" ((o))                                   )")
-print(" \o)_______          _____         ____/")
+print(" ( )_______          _____         ____/")
 print("  |             MAIN MENU            |")
 print("  |                                  |")
 print("  |  1. Start Game                   |")
@@ -23,10 +24,42 @@ print("  |                                  |")
 print("  |  4. Quit                         |")
 print("  |                                  |")
 print("  |____    _______    __  ____    ___|")
-print(" /A\                                  )")
+print(" / )                                  )")
 print("((o))                                  )")
 print(" '-'----------------------------------'")
 
-#tuff
 
-print("\n")
+print("\n\n")
+
+menuChoice = int(input("Choice: "))
+if menuChoice == 1:
+    print("welcome")
+elif menuChoice == 2:
+    print("")
+elif menuChoice == 3:
+    print("╔╦╗╔═╗╦╔╗╔  ╔═╗╦═╗╔═╗╔═╗╦═╗╔═╗╔╦╗╔╦╗╔═╗╦═╗╔═╗ O")
+    time.sleep(1)
+    print("║║║╠═╣║║║║  ╠═╝╠╦╝║ ║║ ╦╠╦╝╠═╣║║║║║║║╣ ╠╦╝╚═╗ ")
+    time.sleep(1)
+    print("╩ ╩╩ ╩╩╝╚╝  ╩  ╩╚═╚═╝╚═╝╩╚═╩ ╩╩ ╩╩ ╩╚═╝╩╚═╚═╝ O")
+    print("\n")
+    time.sleep(1)
+    print("╔╦╗╔═╗╔╗╔╦╔═╗╔═╗  ╔═╗╔╗ ╔═╗╦  ╔═╗╔═╗   ")
+    time.sleep(1)
+    print("║║║║ ║║║║║║  ╠═╣  ╠═╣╠╩╗╠═╣║  ║ ║╚═╗      ")
+    time.sleep(1)
+    print("╩ ╩╚═╝╝╚╝╩╚═╝╩ ╩  ╩ ╩╚═╝╩ ╩╩═╝╚═╝╚═╝     ")
+    print("\n")
+    time.sleep(1)
+    print("╦  ╦╔═╗╔╗╔╔╗╔╔═╗  ╔═╗╔═╗╦═╗╔═╗╔╦╗╔═╗╔═╗  ")
+    time.sleep(1)
+    print("║  ║╠═╣║║║║║║║╣   ╠═╣║ ╦╠╦╝╠═╣║║║║ ║╚═╗   ")
+    time.sleep(1)
+    print("╩═╝╩╩ ╩╝╚╝╝╚╝╚═╝  ╩ ╩╚═╝╩╚═╩ ╩╩ ╩╚═╝╚═╝     ")
+    print("\n")
+    time.sleep(1)
+    print(" ╦╔═╗╔═╗╦ ╦  ╔╗ ╔═╗╔═╗╦ ╦╦╔═╗    ")
+    time.sleep(1)
+    print(" ║╠═╣║  ╚╦╝  ╠╩╗╠═╣║ ╦║ ║║╚═╗    ")
+    time.sleep(1)
+    print("╚╝╩ ╩╚═╝ ╩   ╚═╝╩ ╩╚═╝╚═╝╩╚═╝    ")
